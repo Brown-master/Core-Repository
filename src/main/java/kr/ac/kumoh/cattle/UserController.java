8a7ae7eed3899eef677a8f31e2e39133245dd89d
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2022-10-07 15:49:36 +0900

    ✨add get all user

--- src/main/java/kr/ac/kumoh/cattle/UserController.java
+++ src/main/java/kr/ac/kumoh/cattle/UserController.java
@@ -3,6 +3,8 @@ package kr.ac.kumoh.cattle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -12,6 +14,11 @@ public class UserController {
     @GetMapping("/find")
     public String getUserById(@RequestParam(value = "id",required = false)Integer userId){
         return userService.getUser(userId).get().getFirstName()+userService.getUser(userId).get().getLastName();
+    }
+
+    @GetMapping("/findAll")
+    public List<User> getUserAll(){
+        return userService.getUserAll();
     }
 
     @GetMapping("/insert")
--- src/main/java/kr/ac/kumoh/cattle/UserService.java
+++ src/main/java/kr/ac/kumoh/cattle/UserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,4 +25,6 @@ public class UserService {
     public void deleteUser(Integer userId) {
         userRepository.deleteById(userId);
     }
+
+    public List<User> getUserAll(){return userRepository.findAll();}
 }
